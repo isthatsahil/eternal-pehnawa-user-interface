9df070d642921add9d3603e1739ab62c69181447
vinayak Khandekar <khandekarvinayak41@gmail.com>
2021-12-01 11:46:28 +0530

    bug fix

--- src/views/products/sarees/SareeContainer.tsx
+++ src/views/products/sarees/SareeContainer.tsx
@@ -1,9 +1,10 @@
 import ProductsComponent from "../ProductsComponent";
-import { useGetProductCategoryQuery } from "../../../redux/services/products";
+import { useGetProductCategoryQuery, useGetProductsQuery } from "../../../redux/services/products";
 import { useSelector } from "react-redux";
 import { applyFilter } from "../../../utils/utils";
 
 const SareeContainer = () => {
+  useGetProductsQuery(200);
   let { data, error, isLoading } = useGetProductCategoryQuery("saree");
   const filter = useSelector((state: any) => state.filter);
   data = applyFilter(data, filter);
